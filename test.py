4c346a5c3b8da95a35a557ddcff99410045be274
Daniel Petti <djpetti@gmail.com>
2014-06-29 22:32:01 -0700

    Add in actual code I've been working on.

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import control
+
+wheels = control.Wheels()
+wheels.safe_drive(500, 500, 200)
+wheels.safe_drive(300, -300, 50)
+wheels.safe_drive(-300, 300, 100)
+wheels.safe_drive(-500, -500, 200)
